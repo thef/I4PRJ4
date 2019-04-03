f00b0c8fdba20d478e35a389e34c8ac8b6773437
ali <alielkaatib@hotmai.com>
2019-04-03 13:59:10 +0200

    database til cart

---
+++ myWebApp/Pages/Cart/cart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using myWebApp;
+
+namespace myWebApp.Pages.Cart
+{
+    public class cart
+    {
+        [Key]
+        public string ItemId { get; set; }
+
+        public string UserId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public  int ProductId { get; set; }
+
+        public Product.Product Product { get; set; }
+    }
+}
