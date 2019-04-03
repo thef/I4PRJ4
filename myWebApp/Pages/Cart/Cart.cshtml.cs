b33b788deb2bd02edbdb4b136424802d04b95c88
Oliver <oliver_mei@hotmail.com>
2019-04-03 13:41:13 +0200

    Added cart razer page

---
+++ myWebApp/Pages/Cart/Cart.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+//For using folders.
+using myWebApp.Pages.Product;
+using myWebApp.Pages.Account;
+
+namespace myWebApp.Pages.Cart
+{
+    public class CartModel : PageModel
+    {
+        private readonly UserManager<ApplicationDbUser> _userManager;
+        public CartModel(UserManager<ApplicationDbUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task OnGetAsync()
+        {
+            var user = _userManager.FindByEmailAsync(User.Identity.Name);
+
+        }
+    }
+}
