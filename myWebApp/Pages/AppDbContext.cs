39e45ceb8044f4edd7e626f720e9a22324f9ad96
SWTHOLD18 <47780943+SWTHOLD18@users.noreply.github.com>
2019-05-19 23:06:43 +0200

    Changes connectionstring

--- myWebApp/Pages/AppDbContext.cs
+++ myWebApp/Pages/AppDbContext.cs
@@ -31,8 +31,8 @@ namespace myWebApp.Pages.Product
         {
             //optionsBuilder.UseSqlite("Filename=WebStoreDatabase.db");
             //optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=PRJ4;Integrated Security=True");
-            //optionsBuilder.UseSqlServer("Server=tcp:dabexercise.database.windows.net,1433;Initial Catalog=DAB;Persist Security Info=False;User ID=DAB;Password=Qwerty1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            optionsBuilder.UseSqlServer("Server=127.0.0.1,1433; Database=PRJ4; User Id=SA; Password=D15987532147er!");
+            optionsBuilder.UseSqlServer("Server=tcp:i4prj4.database.windows.net,1433;Initial Catalog=I4PRJ4;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
+            //optionsBuilder.UseSqlServer("Server=127.0.0.1,1433; Database=PRJ4; User Id=SA; Password=D15987532147er!");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
